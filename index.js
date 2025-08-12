d4604f509e8df66318620ebbb14187a65a5c7ce5
toprakyazilimci <topraksener541@gmail.com>
2025-08-12 14:21:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
 
 app.get("/", (req, res) => {
-  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  const logLine = `${ip} - ${new Date().toISOString()}\n`;
-  fs.appendFile("ip_log.txt", logLine, (err) => {
-    if (err) console.error("Dosya yazma hatası:", err);
-  });
-  res.redirect("https://ornek.com"); // Yönlendirmek istediğin linki buraya yaz
+  res.send("Sunucu çalışıyor!");
 });
 
 const PORT = process.env.PORT || 3000;
